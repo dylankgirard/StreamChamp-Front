2e0d9a3938b81428449118c0e61499dca2f9061f
dylankgirard <dylankgirard@gmail.com>
2020-09-09 16:07:01 -0700

    Created simple Login form

--- src/Header.js
+++ src/Header.js
@@ -3,32 +3,30 @@ import { Link } from 'react-router-dom';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import './Header.css';
 
-class Header extends Component {
-	render() {
-		return (
-			<Navbar className='header' variant='dark' expand='lg' sticky='top'>
-				<Navbar.Brand id='main-brand' as={Link} to='/'>
-					Stream-Champ
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls='basic-navbar-nav' />
-				<Navbar.Collapse id='basic-navbar-nav'>
-					<Container className='justify-content-end mr-0 pr-0'>
-						<Nav>
-							<Nav.Link id='brand' as={Link} to='/login'>
-								Log-In
-							</Nav.Link>
-							<Nav.Link id='brand' as={Link} to='/signup'>
-								Sign-Up
-							</Nav.Link>
-							<Nav.Link id='brand' as={Link} to='/browse'>
-								Browse
-							</Nav.Link>
-						</Nav>
-					</Container>
-				</Navbar.Collapse>
-			</Navbar>
-		);
-	}
+function Header() {
+	return (
+		<Navbar className='header' variant='dark' expand='lg' sticky='top'>
+			<Navbar.Brand id='main-brand' as={Link} to='/'>
+				Stream-Champ
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls='basic-navbar-nav' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Container className='justify-content-end mr-0 pr-0'>
+					<Nav>
+						<Nav.Link id='brand' as={Link} to='/login'>
+							Log-In
+						</Nav.Link>
+						<Nav.Link id='brand' as={Link} to='/signup'>
+							Sign-Up
+						</Nav.Link>
+						<Nav.Link id='brand' as={Link} to='/browse'>
+							Browse
+						</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
 export default Header;
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 class LoginPage extends Component {
 	render() {
 		return (
 			<Container>
-				<p>Welcome to the Login Page!</p>
+				<h1>Login To Your Account</h1>
+				<Form>
+					<Form.Group controlId='formBasicText'>
+						<Form.Label>User Name</Form.Label>
+						<Form.Control type='text' placeholder='Enter Your User Name' />
+					</Form.Group>
+					<Form.Group controlId='formBasicPassword'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type='password' placeholder='Password' />
+					</Form.Group>
+					<Button variant='primary' type='submit'>
+						Submit
+					</Button>
+				</Form>
 			</Container>
 		);
 	}
