d7ab72d2f94269e953fa9d6a3c8ddad582464e20
dylankgirard <dylankgirard@gmail.com>
2020-09-09 15:56:02 -0700

    Created basic Create form

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 class SignUpPage extends Component {
 	render() {
 		return (
 			<Container>
-				<p>Welcome to the SignUp Page!</p>
+				<h1>Create an Account</h1>
+				<Form>
+					<Form.Group controlId='formBasicText'>
+						<Form.Label>User Name</Form.Label>
+						<Form.Control type='text' placeholder='Enter A User Name' />
+					</Form.Group>
+					<Form.Group controlId='formBasicText'>
+						<Form.Label>Profile Bio</Form.Label>
+						<Form.Control type='text' placeholder='Description' />
+					</Form.Group>
+
+					<Form.Group controlId='formBasicPassword'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type='password' placeholder='Password' />
+					</Form.Group>
+					<Form.Group controlId='formBasicPassword'>
+						<Form.Label>Confirm Password</Form.Label>
+						<Form.Control type='password' placeholder='Password' />
+					</Form.Group>
+
+					<Button variant='primary' type='submit'>
+						Submit
+					</Button>
+				</Form>
 			</Container>
 		);
 	}
