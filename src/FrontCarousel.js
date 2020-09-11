61e6dc2a4515ab3d594ac1200eaf1d7304820dfb
dylankgirard <dylankgirard@gmail.com>
2020-09-11 12:07:38 -0700

    Create page Posts to the DB on submit

--- src/FrontCarousel.js
+++ src/FrontCarousel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
 import { TwitchEmbed } from 'react-twitch-embed';
--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import { Container, Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
 
 class SignUpPage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: '',
+			bio: '',
+		};
+	}
+
+	handleNameChange = (e) => {
+		this.setState({ name: e.target.value });
+	};
+
+	handleBioChange = (e) => {
+		this.setState({ bio: e.target.value });
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.post(`http://localhost:8000/users/`, {
+				name: this.state.name,
+				bio: this.state.bio,
+			})
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				this.setState({ err: err });
+			});
+			
+		
+	};
+
 	render() {
+		
 		return (
 			<Container>
 				<h1>Create an Account</h1>
 				<Form>
 					<Form.Group controlId='formBasicText'>
 						<Form.Label>User Name</Form.Label>
-						<Form.Control type='text' placeholder='Enter A User Name' />
+						<Form.Control
+							type='text'
+							placeholder='Enter A User Name'
+							onChange={this.handleNameChange}
+						/>
 					</Form.Group>
 					<Form.Group controlId='formBasicText'>
 						<Form.Label>Profile Bio</Form.Label>
-						<Form.Control type='text' placeholder='Description' />
+						<Form.Control
+							type='text'
+							placeholder='Description'
+							onChange={this.handleBioChange}
+						/>
 					</Form.Group>
 
 					{/* <Form.Group controlId='formBasicPassword'>
@@ -26,7 +70,7 @@ class SignUpPage extends Component {
 						<Form.Control type='password' placeholder='Password' />
 					</Form.Group> */}
 
-					<Button variant='primary' type='submit'>
+					<Button variant='primary' type='submit' onClick={this.handleSubmit}>
 						Submit
 					</Button>
 				</Form>
