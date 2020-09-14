9d97bc51e1fc7b9fd1ddc2fb5d1e42b02eee8822
dylankgirard <dylankgirard@gmail.com>
2020-09-13 18:07:20 -0700

    Seemingly fixed the PUT problem with ID in PUT request

--- src/UpdateModal.js
+++ src/UpdateModal.js
@@ -9,6 +9,7 @@ function UpdateModal(props) {
 	const handleSubmit = () => {
 		axios
 			.put(`https://stream-champ-back.herokuapp.com/users/${user.id}`, {
+				id: user.id,
 				name: props.name,
 				bio: props.bio,
 				favorites: user.favorites,
